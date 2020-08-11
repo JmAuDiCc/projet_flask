60385665e385f0275d6ef34486a4a4cec39bc06f
jmA <jeanmarieaudic@gmail.com>
2020-08-11 14:24:13 +0200

    fin intro

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template      
 
 app = Flask(__name__)
 
-#@app.route('/', methods=['POST', 'GET'])
-@app.route('/')
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
 
-def index():
-    return "Helloword!"
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
-    
+  #We made two new changes
 
